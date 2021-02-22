bc919079a62ce0c85d81671da14f3df047a737b3
Ndumiso Mguni <Ndumiso29@gmail.com>
2021-02-22 10:56:17 -0500

    new projects added, old ones removed

--- src/components/1-portfolio.js
+++ src/components/1-portfolio.js
@@ -6,29 +6,14 @@ import "../styles/portfolio.scss"
 const Portfolio = () => {
   const data = useStaticQuery(graphql`
     query {
-      bookAppSave: file(relativePath: { eq: "BookAppSave.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      chatApp: file(relativePath: { eq: "chatApp.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      innerBlog: file(relativePath: { eq: "innerBlog.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-
       shoeShop: file(relativePath: { eq: "shoe_shop.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      shoeShopshoe: file(relativePath: { eq: "shoe_shop_shoe.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid
@@ -37,22 +22,6 @@ const Portfolio = () => {
       }
 
       notesApp: file(relativePath: { eq: "notes-app.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-
-      blogpage: file(relativePath: { eq: "blog.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-
-      blogposts: file(relativePath: { eq: "blog_posts.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid
@@ -75,7 +44,30 @@ const Portfolio = () => {
           }
         }
       }
+
       inps: file(relativePath: { eq: "inps.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      sigma: file(relativePath: { eq: "sigma.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      orthodontics: file(relativePath: { eq: "orthodontics.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      soleScience: file(relativePath: { eq: "soleScience.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid
@@ -89,9 +81,237 @@ const Portfolio = () => {
     <div id="portfolio" className="portfolio__container">
       <div className="portfolio__title">Projects</div>
 
-      {/*  DOUBLE IMG CONTAINER*/}
+      {/* Top container */}
       <div className="portfolio__content">
         <div className="portfolio__image__container ">
+          <div className="image__container">
+            <Img
+              alt="inps"
+              className="image"
+              fluid={data.inps.childImageSharp.fluid}
+            />
+          </div>
+        </div>
+        <div className="portfolio__details__container">
+          <div className="details__title">Inps </div>
+          <div className="details__description">
+            <ul>
+              <li>
+                {" "}
+                My main task was to transform PDF’s into working pages for the
+                Canadian, US, and Mexican Sites
+              </li>
+              <li>
+                {" "}
+                Page rebuilds were completed for all the Rail, Graphic, Transit,
+                OEM, and Fleet Solutions pages.
+              </li>
+              <li>Optimized images in compliance with SEO standards.</li>
+              <li>
+                Screaming Frog crawler to audit sites for common SEO issues by
+                extracting data & auditing for common SEO issues. (Find broken
+                Links audit redirects, discover duplicate content, analyze page
+                titles & MetaData just to name a few)
+              </li>
+              <li>
+                <div className="details__stack"> Built With Drupal</div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="details__links">
+            <a
+              className="link"
+              href="https://inps.net/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Site Link
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Bottom  */}
+
+      {/* Top container */}
+      <div className="portfolio__content">
+        <div className="portfolio__image__container ">
+          <div className="image__container image__container__cyan__bg ">
+            <Img
+              alt="Sigma Assessment Systems"
+              className="image"
+              fluid={data.sigma.childImageSharp.fluid}
+            />
+          </div>
+        </div>
+        <div className="portfolio__details__container">
+          <div className="details__title">Sigma Assessment Systems </div>
+          <div className="details__description">
+            <ul>
+              <li>
+                {" "}
+                My main task was to transform PDF’s into working pages in the
+                succession planning section.
+              </li>
+              <li>
+                {" "}
+                Upload content to the{" "}
+                <a
+                  className="teal-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.sigmaassessmentsystems.com/blog-posts/"
+                >
+                  blog page
+                </a>{" "}
+                along with optimizing images.
+              </li>
+
+              <li>
+                Screaming Frog crawler to audit sites for common SEO issues by
+                extracting data & auditing for common SEO issues. (Find broken
+                Links audit redirects, discover duplicate content, analyze page
+                titles & MetaData just to name a few).
+              </li>
+              <li>
+                <div className="details__stack"> Built With WordPress</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details__links">
+            <a
+              className="link"
+              href="https://www.sigmaassessmentsystems.com/solutions/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Site Link
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Bottom  */}
+
+      {/* Top container */}
+      <div className="portfolio__content">
+        <div className="portfolio__image__container ">
+          <div className="image__container image__container__red__bg ">
+            <Img
+              alt="Foley Wilde
+Orthodontics"
+              className="image"
+              fluid={data.orthodontics.childImageSharp.fluid}
+            />
+          </div>
+        </div>
+        <div className="portfolio__details__container">
+          <div className="details__title">Foley Wilde Orthodontics </div>
+          <div className="details__description">
+            <ul>
+              <li>
+                My main task was to rebuild pages and insert new / updated
+                content
+              </li>
+              <li>
+                {" "}
+                Upload content to the{" "}
+                <a
+                  className="teal-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.foleywildeortho.com/blog/"
+                >
+                  blog page
+                </a>{" "}
+                along with optimizing images.
+              </li>
+
+              <li>
+                Screaming Frog crawler to audit sites for common SEO issues by
+                extracting data & auditing for common SEO issues. (Find broken
+                Links audit redirects, discover duplicate content, analyze page
+                titles & MetaData just to name a few)
+              </li>
+              <li>
+                <div className="details__stack"> Built With WordPress</div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="details__links">
+            <a
+              className="link"
+              href="https://www.foleywildeortho.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Site Link
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Bottom  */}
+      {/* Top container */}
+      <div className="portfolio__content">
+        <div className="portfolio__image__container ">
+          <div className="image__container image__container__teal__bg">
+            <Img
+              alt="soleScience"
+              className="image"
+              fluid={data.soleScience.childImageSharp.fluid}
+            />
+          </div>
+        </div>
+        <div className="portfolio__details__container">
+          <div className="details__title">SoleScience </div>
+          <div className="details__description">
+            <ul>
+              <li>
+                My main task was to rebuild pages and insert new / updated
+                content
+              </li>
+              <li>
+                Upload content to the{" "}
+                <a
+                  className="teal-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.solescience.ca/blog"
+                >
+                  blog page
+                </a>
+              </li>
+              <li>Optimized images in compliance with SEO standards.</li>
+              <li>
+                Screaming Frog crawler to audit sites for common SEO issues by
+                extracting data & auditing for common SEO issues. (Find broken
+                Links audit redirects, discover duplicate content, analyze page
+                titles & MetaData just to name a few){" "}
+              </li>
+              <li>
+                <div className="details__stack"> Built With WordPress</div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="details__links">
+            <a
+              className="link"
+              href="https://www.solescience.ca/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Site Link
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Bottom  */}
+
+      <div className="portfolio__title">personal Projects</div>
+      {/*  DOUBLE IMG CONTAINER*/}
+      <div className="portfolio__content">
+        <div className="portfolio__image__container  image__container__orange__bg">
           <div className="image__container">
             <Img
               alt="Theme store"
@@ -101,7 +321,7 @@ const Portfolio = () => {
           </div>
         </div>
 
-        <div className="portfolio__image__container ">
+        <div className="portfolio__image__container  image__container__orange__bg">
           <div className="image__container">
             <Img
               alt="theme cart page"
@@ -141,54 +361,9 @@ const Portfolio = () => {
       </div>
       {/* Bottom  */}
 
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-
-      {/* Top container */}
+      {/*  DOUBLE IMG CONTAINER*/}
       <div className="portfolio__content">
-        <div className="portfolio__image__container ">
-          <div className="image__container">
-            <Img
-              alt="shoe shop"
-              className="image"
-              fluid={data.inps.childImageSharp.fluid}
-            />
-          </div>
-        </div>
-        <div className="portfolio__details__container">
-          <div className="details__title">inps </div>
-          <div className="details__description">
-            Make sure images comply with SEO standards. Under 100kb and contain
-            Alt texts Each page has an SEO title and Meta Description Run
-            Screaming Frog crawler to audit sites for common SEO issues by
-            extracting data & auditing for common SEO issues. (Find broken Links
-            audit redirects, discover duplicate content, analyze page titles &
-            MetaData just to name a few) My main task was to transform PDF’s
-            into working pages in the succession planning section.
-          </div>
-          <div className="details__stack">Drupal</div>
-          <div className="details__links">
-            <a
-              className="link"
-              href="https://inps.net/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Site Link
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* Bottom  */}
-
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-      {/* --------------------------------------------------------------------------------------------------------------------------- */}
-
-      {/* Top container */}
-      <div className="portfolio__content">
-        <div className="portfolio__image__container image__container__red__bg">
+        <div className="portfolio__image__container image__container__cyan__bg  ">
           <div className="image__container">
             <Img
               alt="shoe shop"
@@ -197,8 +372,19 @@ const Portfolio = () => {
             />
           </div>
         </div>
-        <div className="portfolio__details__container">
-          <div className="details__title"> Shoe Store </div>
+
+        <div className="portfolio__image__container image__container__cyan__bg  ">
+          <div className="image__container">
+            <Img
+              alt="shoe shop item"
+              className="image"
+              fluid={data.shoeShopshoe.childImageSharp.fluid}
+            />
+          </div>
+        </div>
+
+        <div className="double__image">
+          <div className="details__title"> Theme Store </div>
           <div className="details__description">
             E-Commerce Website. Logged in user can add/remove items to the store
             and buy them using PayPal API. <br />{" "}
@@ -206,9 +392,7 @@ const Portfolio = () => {
               Login Info <br /> Email: johnwick@gmail.com PW: john1234
             </span>
           </div>
-          <div className="details__stack">
-            React + Redux - Node - Express - MongoDB (MERN stack)
-          </div>
+          React + Redux - Node - Express - MongoDB (MERN stack)
           <div className="details__links">
             <a
               className="link"
@@ -222,94 +406,6 @@ const Portfolio = () => {
             <a
               className="link"
               href="https://calm-eyrie-12447.herokuapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* Bottom  */}
-
-      {/* Top container */}
-      <div className="portfolio__content">
-        <div className="portfolio__image__container">
-          <div className="image__container">
-            <Img
-              alt="real time chat app login"
-              className="image"
-              fluid={data.chatApp.childImageSharp.fluid}
-            />
-          </div>
-        </div>
-        <div className="portfolio__details__container">
-          <div className="details__title"> Realtime Chat Application</div>
-          <div className="details__description">
-            I used Socket.io to manage real-time web socket connections to a
-            Node.js server that will allow you to communicate real-time chat
-            messages to all clients connected to a single server.
-          </div>
-          <div className="details__stack">
-            React - Express - Axios - Node.js - Socket.io
-          </div>
-          <div className="details__links">
-            <a
-              className="link"
-              href="https://github.com/nmguni/-realtime-chat-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code
-            </a>{" "}
-            |{" "}
-            <a
-              className="link"
-              href="https://rick-morty-chat-app.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* Bottom  */}
-
-      {/* Top container */}
-      <div className="portfolio__content">
-        <div className="portfolio__image__container image__container__cyan__bg">
-          <div className="image__container">
-            <Img
-              alt="real time chat app login"
-              className="image"
-              fluid={data.notesApp.childImageSharp.fluid}
-            />
-          </div>
-        </div>
-        <div className="portfolio__details__container">
-          <div className="details__title"> Notes App </div>
-          <div className="details__description">
-            This app allows users to post notes or reminders. Images can be
-            added to notes to make them stand out. Notes and be deleted or
-            edited as well as likes can be added to the notes.
-          </div>
-          <div className="details__stack">
-            React + Redux - Node - Express - MongoDB (MERN stack)
-          </div>
-          <div className="details__links">
-            <a
-              className="link"
-              href="https://github.com/nmguni/notes-app-client"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code
-            </a>{" "}
-            |{" "}
-            <a
-              className="link"
-              href="https://mern-notes-app.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
