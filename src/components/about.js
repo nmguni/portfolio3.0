13c722d03f6c81a3986f7c522c963251c64ee866
Ndumiso Mguni <Ndumiso29@gmail.com>
2021-03-29 21:01:32 -0400

    about section ui change

--- src/components/about.js
+++ src/components/about.js
@@ -1,75 +1,62 @@
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faServer,
-  faDesktop,
-  faCode,
-  faEdit,
-} from "@fortawesome/free-solid-svg-icons"
+import "../styles/about.scss"
 
 const About = () => {
   return (
     <div id="about" className="about">
-      <div className="about__wrapper">
-        <div className="about__text">
-          <div className="about__text-top"> who am i...</div>
-          <div className="about__text-mid">A few words about myself</div>
-
-          <div className="about__text-btm">
-            {" "}
-            I’m a Frontend Web Developer based in Ontario with 4 years
-            experience (there is still a lot for me to learn!). My love for both
-            technology and design make the web my ideal playground. Outside of
-            developing websites, I enjoy reading{" "}
-            <span>
-              (Currently reading Stalingrad The Fateful Siege: 1942 - 1943)
-            </span>{" "}
-            , the gym, and having BBQ's with friends.
+      <div className="about__container">
+        <div className="text__container">
+          <div
+            style={{
+              background: "rgba(255, 255, 255, 0.24)",
+              padding: "1rem",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div className="text__container--title">
+              A Few Words About Myself
+            </div>
+            <div className="text__container--paragraph">
+              I’m a Frontend Web Developer based in Ontario with 4 years
+              experience (there is still a lot for me to learn!). My love for
+              both technology and design make the web my ideal playground.
+              Outside of developing websites, I enjoy reading (Currently reading
+              Stalingrad The Fateful Siege: 1942 - 1943) , the gym, and having
+              BBQ's with friends.
+            </div>
           </div>
         </div>
-        <div className="about__box">
-          <div className="box">
-            {" "}
-            <div className="box__img">
-              {" "}
-              <FontAwesomeIcon icon={faDesktop} size="2x" />
-            </div>{" "}
-            <div className="name">Frontend</div>{" "}
-            <div className="description">
+        <div className="stack__container">
+          <ul>
+            <li className="stack__title">Front-End</li>
+            <li className="stack__tech">
               HTML5 - CSS3 SASS - JavaScript - React - Redux
-            </div>{" "}
-          </div>
-          <div className="box">
-            {" "}
-            <div className="box__img">
-              {" "}
-              <FontAwesomeIcon icon={faServer} size="2x" />
-            </div>{" "}
-            <div className="name">Backend / Developing</div>{" "}
-            <div className="description">Node - MongoDB - Express </div>{" "}
-          </div>
-          <div className="box">
-            {" "}
-            <div className="box__img">
-              {" "}
-              <FontAwesomeIcon icon={faEdit} size="2x" />
-            </div>{" "}
-            <div className="name">Prototyping / Version Controll</div>{" "}
-            <div className="description">Figma Git - Git Hub</div>{" "}
-          </div>
-          <div className="box">
-            {" "}
-            <div className="box__img">
-              {" "}
-              <FontAwesomeIcon icon={faCode} size="2x" />
-            </div>{" "}
-            <div className="name">CMS / CSS Frameworks</div>{" "}
-            <div className="description">
-              WordPress - Material UI - Bootstrap - Gatsby
-            </div>{" "}
-          </div>
+            </li>
+          </ul>
+          <ul>
+            <li className="stack__title">Back-End</li>
+            <li className="stack__tech">Node - MongoDB - Express</li>
+          </ul>
+          <ul>
+            <li className="stack__title">Prototyping</li>
+            <li className="stack__tech">Figma</li>
+          </ul>
+          <ul>
+            <li className="stack__title">CMS</li>
+            <li className="stack__tech">WordPress - Drupal</li>
+          </ul>
+          <ul>
+            <li className="stack__title">Css Frameworks</li>
+            <li className="stack__tech">Material UI - Bootstrap - Gatsby</li>
+          </ul>
+          <ul>
+            <li className="stack__title"> Version Controll</li>
+            <li className="stack__tech">Git - Git Hub</li>
+          </ul>
         </div>
-        {/* ------------------ */}
       </div>
     </div>
   )
