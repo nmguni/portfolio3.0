d819fc5599a735ad790bdc639c788dfe777948ae
Ndumiso Mguni <Ndumiso29@gmail.com>
2021-03-30 11:27:33 -0400

    about section updates

--- src/components/about.js
+++ src/components/about.js
@@ -6,34 +6,22 @@ const About = () => {
     <div id="about" className="about">
       <div className="about__container">
         <div className="text__container">
-          <div
-            style={{
-              background: "rgba(255, 255, 255, 0.24)",
-              padding: "1rem",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <div className="text__container--title">
-              A Few Words About Myself
-            </div>
-            <div className="text__container--paragraph">
-              I’m a Frontend Web Developer based in Ontario with 4 years
-              experience (there is still a lot for me to learn!). My love for
-              both technology and design make the web my ideal playground.
-              Outside of developing websites, I enjoy reading (Currently reading
-              Stalingrad The Fateful Siege: 1942 - 1943) , the gym, and having
-              BBQ's with friends.
-            </div>
+          <div className="text__container--title">A Few Words About Myself</div>
+          <div className="text__container--paragraph">
+            I’m a Frontend Web Developer based in Ontario with 4 years
+            experience (there is still a lot for me to learn!). My love for both
+            technology and design make the web my ideal playground. Outside of
+            developing websites, I enjoy reading (Currently reading Stalingrad
+            The Fateful Siege: 1942 - 1943) , the gym, and having BBQ's with
+            friends.
           </div>
         </div>
         <div className="stack__container">
           <ul>
             <li className="stack__title">Front-End</li>
             <li className="stack__tech">
-              HTML5 - CSS3 SASS - JavaScript - React - Redux
+              HTML5 - CSS3 - SASS - Styled Components - JavaScript - React /
+              Gatsby
             </li>
           </ul>
           <ul>
@@ -49,12 +37,12 @@ const About = () => {
             <li className="stack__tech">WordPress - Drupal</li>
           </ul>
           <ul>
-            <li className="stack__title">Css Frameworks</li>
-            <li className="stack__tech">Material UI - Bootstrap - Gatsby</li>
+            <li className="stack__title">Frameworks</li>
+            <li className="stack__tech">Material UI - Bootstrap</li>
           </ul>
           <ul>
             <li className="stack__title"> Version Controll</li>
-            <li className="stack__tech">Git - Git Hub</li>
+            <li className="stack__tech">Git / GitHub</li>
           </ul>
         </div>
       </div>
